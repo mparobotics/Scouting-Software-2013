8d3b9959064d6a9f4145e8dd07123e33ead1fb86
Brendan Boyle <brendan@dbztech.com>
2013-02-17 18:25:01 -0600

    Added multiple views and base javascript

--- scripts.js
+++ scripts.js
@@ -1,1 +1,30 @@
 //JavaScript Document
+
+/*
+Correct Class Definition:
+
+
+function Apple (type) {
+    this.type = type;
+    this.color = "red";
+    this.getInfo = function() {
+        return this.color + ' ' + this.type + ' apple';
+    };
+}
+
+*/
+
+
+
+function core() {
+    this.showId = function(id) {
+        document.getElementById(id).style.display = "block";
+    };
+    
+    this.hideId = function(id) {
+        document.getElementById(id).style.display = "none";
+    };
+}
+
+
+var Core = new core;
