94d87e321eba1973adcb110e0dba9daf71554a01
Brendan Boyle <brendan@dbztech.com>
2013-02-18 21:15:49 -0600

    Added data set creation and logging

--- scripts.js
+++ scripts.js
@@ -60,6 +60,10 @@ function data() {
     this.team1 = "";
     this.team2 = "";
     this.team3 = "";
+    this.matchNumber = 0;
+    this.teamData = localStorage.getItem('teamData');
+    //Team Number, Match Number, Thoughts, Shooting, Lifting, Assisting, Penalties, Comments
+    this.currentTeamData = new Array([0,0,0,0,0,0,"N/A","N/A"], [0,0,0,0,0,0,"N/A","N/A"], [0,0,0,0,0,0,"N/A","N/A"]);
     
     this.checkDeviceId = function() {
         if (Data.id > 0) {
@@ -80,11 +84,51 @@ function data() {
         Data.team1 = document.getElementById('team1').value;
         Data.team2 = document.getElementById('team2').value;
         Data.team3 = document.getElementById('team3').value;
+        Data.matchNumber = document.getElementById('matchNumber').value;
         document.getElementById('teamStat1Label').innerHTML = "Team: "+Data.team1;
         document.getElementById('teamStat2Label').innerHTML = "Team: "+Data.team2;
         document.getElementById('teamStat3Label').innerHTML = "Team: "+Data.team3;
+        document.getElementById('teamMatchLabel').innerHTML = "Match: "+Data.matchNumber;
         View.pushView(3);
         return true;
+    };
+    
+    this.createData = function() {
+        Data.currentTeamData[0][0] = Data.team1;
+        Data.currentTeamData[1][0] = Data.team2;
+        Data.currentTeamData[2][0] = Data.team3;
+        Data.currentTeamData[0][1] = Data.matchNumber;
+        Data.currentTeamData[1][1] = Data.matchNumber;
+        Data.currentTeamData[2][1] = Data.matchNumber;
+        Data.currentTeamData[0][2] = document.getElementById('scoreDetailObject11').value;
+        Data.currentTeamData[1][2] = document.getElementById('scoreDetailObject21').value;
+        Data.currentTeamData[2][2] = document.getElementById('scoreDetailObject31').value;
+        Data.currentTeamData[0][3] = document.getElementById('scoreDetailObject12').value;
+        Data.currentTeamData[1][3] = document.getElementById('scoreDetailObject22').value;
+        Data.currentTeamData[2][3] = document.getElementById('scoreDetailObject32').value;
+        Data.currentTeamData[0][4] = document.getElementById('scoreDetailObject13').value;
+        Data.currentTeamData[1][4] = document.getElementById('scoreDetailObject23').value;
+        Data.currentTeamData[2][4] = document.getElementById('scoreDetailObject33').value;
+        Data.currentTeamData[0][5] = document.getElementById('scoreDetailObject14').value;
+        Data.currentTeamData[1][5] = document.getElementById('scoreDetailObject24').value;
+        Data.currentTeamData[2][5] = document.getElementById('scoreDetailObject34').value;
+        Data.currentTeamData[0][6] = document.getElementById('scoreDetailObject15').value;
+        Data.currentTeamData[1][6] = document.getElementById('scoreDetailObject25').value;
+        Data.currentTeamData[2][6] = document.getElementById('scoreDetailObject35').value;
+        Data.currentTeamData[0][7] = document.getElementById('scoreDetailObject16').value;
+        Data.currentTeamData[1][7] = document.getElementById('scoreDetailObject26').value;
+        Data.currentTeamData[2][7] = document.getElementById('scoreDetailObject36').value;
+    };
+        
+    this.saveData = function() {
+        
+        if (Data.teamData == null) {
+            Data.teamData = Data.currentTeamData.join(',');
+        } else {
+            Data.teamData = Data.teamData+Data.currentTeamData.join(',');
+        }
+        
+        localStorage.setItem("teamData", Data.teamData);
     };
 }
 
