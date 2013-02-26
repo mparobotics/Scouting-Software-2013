bfa92fb6563fdda1653a52915d6841382143d853
Brendan Boyle <brendancboyle@me.com>
2013-02-26 13:38:05 -0600

    Fixed parse script

--- TwitterParse/parse.php
+++ TwitterParse/parse.php
@@ -78,8 +78,8 @@ for ( $i = 0; $i < 20; $i++) {
   $matchRTele = $itemContent[31];
   $matchBTele = $itemContent[33];
      
-     $sql = "INSERT INTO `scouting2013`.`matchdata` (`Event`, `MatchType`, `MatchNumber`, `RedFinal`, `BlueFinal`, `Red1`, `Red2`, `Red3`, `Blue1`, `Blue2`, `Blue3`, `RedClimb`, `BlueClimb`, `RedFoul`, `BlueFoul`, `RedAuto`, `BlueAuto`, `RedTeleop`, `BlueTeleop`, `Id`, `Timestamp`) VALUES ('".$matchLocation."', \'".$matchLocation."', \'".$matchLocation."', \'".$matchLocation."', \'43\', \'12\', \'1\', \'6\', \'11\', \'9\', \'7\', \'40\', \'30\', \'0\', \'0\', \'72\', \'6\', \'77\', \'7\', NULL, NOW());";
-  
+     $sql = "INSERT INTO `scouting2013`.`matchdata` (`Event`, `MatchType`, `MatchNumber`, `RedFinal`, `BlueFinal`, `Red1`, `Red2`, `Red3`, `Blue1`, `Blue2`, `Blue3`, `RedClimb`, `BlueClimb`, `RedFoul`, `BlueFoul`, `RedAuto`, `BlueAuto`, `RedTeleop`, `BlueTeleop`, `Id`, `Timestamp`) VALUES ('".$matchLocation."', '".$matchType."', '".$matchNum."', '".$matchRF."', '".$matchBF."', '".$matchROne."', '".$matchRTwo."', '".$matchRThr."', '".$matchBOne."', '".$matchBTwo."', '".$matchBThr."', '".$matchRC."', '".$matchBC."', '".$matchRPen."', '".$matchBPen."', '".$matchRAuto."', '".$matchBAuto."', '".$matchRTele."', '".$matchBTele."', NULL, NOW());";
+     echo $sql;
      echo('<tr><td>'.$matchLocation.'</td><td>'.$matchType.'</td><td>'.$matchNum.'</td><td>'.$matchRF.'</td><td>'.$matchBF.'</td><td>'.$matchROne.'</td><td>'.$matchRTwo.'</td><td>'.$matchRThr.'</td><td>'.$matchBOne.'</td><td>'.$matchBTwo.'</td><td>'.$matchBThr.'</td><td>'.$matchRC.'</td><td>'.$matchBC.'</td><td>'.$matchRPen.'</td><td>'.$matchBPen.'</td><td>'.$matchRAuto.'</td><td>'.$matchBAuto.'</td><td>'.$matchRTele.'</td><td>'.$matchBTele.'</td></tr>');
      
 
--- TwitterParse/scouting2013.sql
+++ TwitterParse/scouting2013.sql
@@ -1,13 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.0.0-beta1
+-- version 3.5.3
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Feb 25, 2013 at 02:06 PM
--- Server version: 5.6.10
--- PHP Version: 5.4.9
+-- Generation Time: Feb 26, 2013 at 11:37 AM
+-- Server version: 5.1.66-community
+-- PHP Version: 5.4.0
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 
@@ -49,8 +49,8 @@ CREATE TABLE IF NOT EXISTS `matchdata` (
   `Id` int(11) NOT NULL AUTO_INCREMENT,
   `Timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   UNIQUE KEY `Id` (`Id`),
-  UNIQUE KEY `Timestamp` (`Timestamp`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
+  KEY `Timestamp` (`Timestamp`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=12 ;
 
 --
 -- Dumping data for table `matchdata`
@@ -58,7 +58,16 @@ CREATE TABLE IF NOT EXISTS `matchdata` (
 
 INSERT INTO `matchdata` (`Event`, `MatchType`, `MatchNumber`, `RedFinal`, `BlueFinal`, `Red1`, `Red2`, `Red3`, `Blue1`, `Blue2`, `Blue3`, `RedClimb`, `BlueClimb`, `RedFoul`, `BlueFoul`, `RedAuto`, `BlueAuto`, `RedTeleop`, `BlueTeleop`, `Id`, `Timestamp`) VALUES
 ('#FRCDEV1', 'P', 6, 50, 179, 5, 10, 8, 4, 2, 3, 30, 30, 0, 0, 0, 60, 20, 89, 1, '2013-02-25 19:47:32'),
-('#FRCDEV1', 'P', 5, 189, 43, 12, 1, 6, 11, 9, 7, 40, 30, 0, 0, 72, 6, 77, 7, 2, '2013-02-25 19:48:46');
+('#FRCDEV1', 'P', 5, 189, 43, 12, 1, 6, 11, 9, 7, 40, 30, 0, 0, 72, 6, 77, 7, 2, '2013-02-25 19:48:46'),
+('#FRCDEV1', 'P', 6, 50, 179, 5, 10, 8, 4, 2, 3, 30, 30, 0, 0, 0, 60, 20, 89, 3, '2013-02-26 19:36:23'),
+('#FRCDEV1', 'P', 5, 189, 43, 12, 1, 6, 11, 9, 7, 40, 30, 0, 0, 72, 6, 77, 7, 4, '2013-02-26 19:36:23'),
+('#FRCDEV1', 'P', 4, 135, 161, 12, 5, 3, 1, 9, 8, 50, 40, 26, 55, 12, 18, 47, 48, 5, '2013-02-26 19:36:23'),
+('#FRCmike', 'P', 3, 0, 0, 1, 10, 9, 2, 3, 102, 0, 0, 0, 0, 0, 0, 0, 0, 6, '2013-02-26 19:36:23'),
+('#FRCDEV1.1', 'P', 4, 43, 46, 4, 5, 2, 1, 3, 102, 0, 20, 43, 26, 0, 0, 0, 0, 7, '2013-02-26 19:36:23'),
+('#FRCDEV1.1', 'P', 4, 0, 0, 4, 5, 2, 1, 3, 102, 0, 0, 0, 0, 0, 0, 0, 0, 8, '2013-02-26 19:36:23'),
+('#FRCNHMA', 'Q', 1, 0, 0, 172, 610, 1729, 811, 151, 131, 0, 0, 0, 0, 0, 0, 0, 0, 9, '2013-02-26 19:36:23'),
+('#FRCNHMA', 'P', 2, 0, 0, 1729, 213, 509, 1991, 166, 1517, 0, 0, 0, 0, 0, 0, 0, 0, 10, '2013-02-26 19:36:23'),
+('#FRCNHMA', 'P', 1, 0, 0, 138, 1512, 1153, 3360, 1973, 4124, 0, 0, 0, 0, 0, 0, 0, 0, 11, '2013-02-26 19:36:23');
 
 -- --------------------------------------------------------
 
@@ -78,14 +87,17 @@ CREATE TABLE IF NOT EXISTS `teamdata` (
   `Id` int(11) NOT NULL AUTO_INCREMENT,
   `Timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   UNIQUE KEY `Id` (`Id`,`Timestamp`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=5 ;
 
 --
 -- Dumping data for table `teamdata`
 --
 
 INSERT INTO `teamdata` (`TeamNumber`, `MatchNumber`, `Overall`, `Shooting`, `Lifting`, `Assisting`, `Penalties`, `Comments`, `Id`, `Timestamp`) VALUES
-(1234, 2, 3, 2, 0, 2, 'Yellow Card', 'N/A', 1, '2013-02-25 20:03:11');
+(1234, 2, 3, 2, 0, 2, 'Yellow Card', 'N/A', 1, '2013-02-25 20:03:11'),
+(6378, 86, 4, 0, 3, 2, 'Red card', 'None', 2, '2013-02-26 00:27:53'),
+(6468, 86, 2, 2, 0, 3, 'None', 'None', 3, '2013-02-26 00:27:53'),
+(7573, 86, 4, 3, 3, 3, 'None', 'Pick me', 4, '2013-02-26 00:27:53');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
