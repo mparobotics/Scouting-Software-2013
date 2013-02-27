c2447820de56ea2489e1132b4dd9255f1a271338
Brendan Boyle <brendan@dbztech.com>
2013-02-27 14:52:48 -0600

    Class warfare

--- classes.php
+++ classes.php
@@ -1,10 +1,10 @@
 <?php
-#PHP Shared Class File
+#Shared Class File
 
 #Basic database interaction class
 class database
 {
-#initialize variables. These are later changed in settings.php
+	#initialize variables. These are later changed in settings.php
     static protected $dbuser = 'scoutinguser';
     static protected $dbpassword = 'hunter3';
     #Do Not Set to localhost, does not work in all environments
@@ -12,111 +12,112 @@ class database
     static protected $database = 'scouting2013';
     static protected $prefix = '';
 
-public static function setcredentials($user, $password, $host = '127.0.0.1', $db = 'scouting2013', $pre = '') {
-     // property declaration
-     $result = 'Credential(s) not set: ';
-     $error = 0;
-     if (is_string($user)) {
-database::$dbuser = $user;
-} else {
-     $error = 1;
-     $result .= '$dbuser ';
-     }
-    
-     if (is_string($password)) {
-database::$dbpassword = $password;
-} else {
-     $error = 1;
-     $result .= '$dbpassword ';
-     }
-    
-     if (!$error) {
-     database::$dbhost = $host;
-     database::$database = $db;
-     database::$prefix = $pre;
-$result = 'Success!';
-} else {
-$result = 'Error!';
-}
+	public static function setcredentials($user, $password, $host = '127.0.0.1', $db = 'scouting2013', $pre = '') {
+    	// property declaration
+    	$result = 'Credential(s) not set: ';
+    	$error = 0;
+    	if (is_string($user)) {
+	    	database::$dbuser = $user;
+	    } else {
+    		$error = 1;
+    		$result .= '$dbuser ';
+    	}
+    	
+    	if (is_string($password)) {
+	    	database::$dbpassword = $password;
+	    } else {
+    		$error = 1;
+    		$result .= '$dbpassword ';
+    	}
+    	
+    	if (!$error) {
+    		database::$dbhost = $host;
+    		database::$database = $db;
+    		database::$prefix = $pre;
+	    	$result = 'Success!';
+	    } else {
+	    	$result = 'Error!';
+	    }
 
-     return $result;
+    	return $result;
     }
     
     public static function sqlconn() {	
-try {
-     $dbh = new PDO('mysql:host='.database::$dbhost.';dbname='.database::$database, database::$dbuser, database::$dbpassword);
-     }
-     catch (PDOException $e) {
-print ("Could not connect to server.n");
-die ("getMessage(): " . $e->getMessage () . "n");
-}
-return $dbh;
-}
+		try {
+    		$dbh = new PDO('mysql:host='.database::$dbhost.';dbname='.database::$database, database::$dbuser, database::$dbpassword);
+    	}
+    	catch (PDOException $e) {
+			print ("Could not connect to server.n");
+			die ("getMessage(): " . $e->getMessage () . "n");
+		}
+		return $dbh;
+	}
 
     // method declaration
     public static function info() {
-     echo "Database: ".database::$database."<br />";
+    	echo "Database: ".database::$database."<br />";
         echo "Database User: ".database::$dbuser."<br />";
         echo "Database Host: ".database::$dbhost."<br />";
         echo "Database Prefix: ".database::$prefix."<br />";
     }
 
     public static function test() {
-     $dbh = database::sqlconn();
-     if ($dbh) {
+    	$dbh = database::sqlconn();
+    	if ($dbh) {
             echo "Connection Successful!";
-     return true;
-     $dbh = NULL;
-     }
-     else {
-     return false;
-     }
+    		return true;
+    		$dbh = NULL;
+    	}
+    	else {
+    		return false;
+    	}
     }
 
     public static function returndata($query) {
-     $dbh = database::sqlconn();
+    	$dbh = database::sqlconn();
 
-// Perform Query
-$result = $dbh->query ($query);
+		// Perform Query
+		$result = $dbh->query ($query);
 
+        if ($result) {
+            while ($row = $result->fetch()) {
+		      return($row);
+            }
+        }
 
-while ($row = $result->fetch()) {
-return($row);
-}
-
-$dbh = NULL;
+		$dbh = NULL;
     }
 
     public static function returnmultiplerows($query) {
-     $dbh = database::sqlconn();
+    	$dbh = database::sqlconn();
 
-$result = $dbh->query ($query);
+		$result = $dbh->query ($query);
+        
+        if ($result) {
+            return $result->fetch();
+        }
 
-return $result;
-
-$dbh = NULL;
+		$dbh = NULL;
     }
 
     public static function writedata($query) {
-     $dbh = database::sqlconn();
+    	$dbh = database::sqlconn();
 
-// Perform Query
-try {
-$result = $dbh->exec ($query);
-}
+		// Perform Query
+		try {
+			$result = $dbh->exec ($query);
+		}
 
-// Check result
-// This shows the actual query sent to MySQL, and the error. Useful for debugging.
-   catch (PDOException $e) {
-print ("Could not connect to server.n");
-die ("getMessage(): " . $e->getMessage () . "n");
-}
+		// Check result
+		// This shows the actual query sent to MySQL, and the error. Useful for debugging.
+  		catch (PDOException $e) {
+			print ("Could not connect to server.n");
+			die ("getMessage(): " . $e->getMessage () . "n");
+		}
 
-return("Query Successful");
+		return("Query Successful");
 
-$dbh = NULL;
+		$dbh = NULL;
     }
 }
-
-
 ?>
