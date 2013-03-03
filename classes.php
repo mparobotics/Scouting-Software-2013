f487dd35a003668c8f2f48a79e168cd4f04e5053
Brendan Boyle <brendancboyle@me.com>
2013-03-03 14:34:11 -0600

    Added match stats

--- classes.php
+++ classes.php
@@ -140,7 +140,8 @@ class analytics
         #SELECT * FROM `matchdata` WHERE `Event` = "#FRCCAMA" AND `MatchType` = "Q" AND `MatchNumber` = 6
         $sql = "SELECT * FROM `matchdata` WHERE `Event` = '#".$event."' AND `MatchType` = '".$type."' AND `MatchNumber` = ".$number;
         $response = database::returnmultiplerows($sql);
-        return $response;
+        echo '<table border="1"><th>Event</th><th>Type</th><th>Number</th><th>Red Score</th><th>Blue Score</th><th>Red Alliance</th><th>Blue Alliance</th><th>Red Climb</th><th>Blue Climb</th><th>Red Auto</th><th>Blue Auto</th><th>Red Teleop</th><th>Blue Teleop</th><th>Red Fouls</th><th>Blue Fouls</th>';
+        echo '<tr><td>'.$response[0].'</td><td>'.$response[1].'</td><td>'.$response[2].'</td><td>'.$response[3].'</td><td>'.$response[4].'</td><td>'.$response[5].', '.$response[6].', '.$response[7].'</td><td>'.$response[8].', '.$response[9].', '.$response[10].'</td><td>'.$response[11].'</td><td>'.$response[12].'</td><td>'.$response[15].'</td><td>'.$response[16].'</td><td>'.$response[17].'</td><td>'.$response[18].'</td><td>'.$response[13].'</td><td>'.$response[14].'</td></tr></table>';
     }
     
     public static function getStat($db,$cond) {
